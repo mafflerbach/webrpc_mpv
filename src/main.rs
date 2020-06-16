7c247221b34f243da53c44ba04c27a27959b1e54
Maren Afflerbach <maren@afflerbach.info>
2020-06-16 23:32:06 +0200

    Document stuff implement play feature

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,93 @@ use rocket::response::content;
 
 mod mpv;
 
-
+/// Resume a video after a pause 
+///
+/// # Example 
+/// ```sh
+/// curl 'http://localhost:8000/resume' 
+/// ```
 #[get("/resume")]
 fn request_resume() -> content::Json<String> {
     return mpv::mpv::event_resume();
 }
 
+/// Pause a video after 
+///
+/// # Example 
+/// ```sh
+/// curl 'http://localhost:8000/pause' 
+/// ```
 #[get("/pause")]
 fn request_pause() -> content::Json<String>  {
     return mpv::mpv::event_pause();
 }
 
+
+/// Load a playlist on the host system
+///
+/// # Example 
+/// ```sh
+/// curl 'http://localhost:8000/playlist?/local/path/to/playlist' 
+/// ```
+/// We are able to load a playlist file.
+/// * target is a absolute path on the host
+///
 #[get("/playlist?<target>")]
 fn request_playlist(target: String) -> content::Json<String> {
     return mpv::mpv::event_play_from_list(target);
 }
 
 
+/// Stopt the actual video and starting a new video based on url/path 
+///
+/// # Example 
+/// ```sh
+/// curl 'http://localhost:8000/load?/local/path/to/playlist' 
+/// curl 'http://localhost:8000/load?target=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DP3UIpTlFtc4y'
+/// ```
+/// We are able to load different targes
+/// * target is a absolute path on the host or a Encoded url 
+///
+/// ## Further examples
+/// ```sh
+/// http://localhost:8000/load?target=/home/joe/Downloads/ytFiles/The Best Way To Practice Chords.webm
+/// http://localhost:8000/load?target=%2Fhome%joe%2FDownloads%2FytFiles%2FThe%20Best%20Way%20To%20Practice%20Chords.webm
+/// http://localhost:8000/load?target=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DP3UIpTlFtc4
+/// ```
+/// will FAIL: 
+/// ```
+/// http://localhost:8000/load?target=https://www.youtube.com/watch?v=P3UIpTlFtc4
+/// ```
+
 #[get("/load?<target>")]
-fn request_load(target: String) -> &'static str {
-    return mpv::mpv::event_play(target);
+fn request_load(target: String) -> content::Json<String>  {
+    return mpv::mpv::event_load(target);
+}
+
+/// Start a new insance of mpv and plays the source target
+///
+/// # Example 
+/// ```sh 
+/// curl 'http://localhost:8000/play?/local/path/to/playlist' 
+/// curl 'http://localhost:8000/play?target=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DP3UIpTlFtc4y'
+/// ```
+/// We are able to load different targes
+/// * target is a absolute path on the host or a Encoded url 
+///
+/// ## Further examples
+/// ```sh
+/// http://localhost:8000/load?target=/home/joe/Downloads/ytFiles/The Best Way To Practice Chords.webm
+/// http://localhost:8000/load?target=%2Fhome%joe%2FDownloads%2FytFiles%2FThe%20Best%20Way%20To%20Practice%20Chords.webm
+/// http://localhost:8000/load?target=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DP3UIpTlFtc4
+/// ```
+/// will FAIL: 
+/// ```sh
+/// http://localhost:8000/load?target=https://www.youtube.com/watch?v=P3UIpTlFtc4
+/// ```
+#[get("/play?<target>")]
+fn request_play(target: String) -> content::Json<String>  {
+    return mpv::mpv::event_load(target);
 }
 
 #[get("/")]
@@ -37,5 +104,11 @@ fn hello() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![ request_load, request_pause, request_resume, request_playlist]).launch();
+    rocket::ignite().mount("/", routes![ 
+                           request_load, 
+                           request_play,
+                           request_pause,
+                           request_resume,
+                           request_playlist
+    ]).launch();
 }
--- src/mpv.rs
+++ src/mpv.rs
@@ -5,11 +5,16 @@ pub mod mpv{
     use std::process::Command;
     use execute::Execute;
     use rocket::response::content;
-
+    
+/// spawn a mpv process with ipc socket server and loads a playlist
+///
+/// # Example 
+/// ```sh
+/// mpv --playlist=/tmp/playlist --input-ipc-server=/tmp/mpvsocket
+/// ```
 pub fn event_play_from_list(target: String) -> content::Json<String> {
-    // http://localhost:8000/load?target=/tmp/playlist
-    let mut mpv = Command::new("mpv");
         let param = format!("--playlist={}", target);
+    let mut mpv = Command::new("mpv");
     mpv.arg(&target).
         arg("--input-ipc-server=/tmp/mpvsocket").
         arg(param);
@@ -20,34 +25,12 @@ pub fn event_play_from_list(target: String) -> content::Json<String> {
 
     }
 
-pub fn event_load(target: String) -> content::Json<String> {
-    // http://localhost:8000/load?target=/home/maren/Downloads/ytFiles/The Best Way To Practice Chords.webm
-    // http://localhost:8000/load?target=%2Fhome%2Fmaren%2FDownloads%2FytFiles%2FThe%20Best%20Way%20To%20Practice%20Chords.webm
-    // http://localhost:8000/load?target=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DP3UIpTlFtc4
-    // 
-    // will FAIL: http://localhost:8000/load?target=https://www.youtube.com/watch?v=P3UIpTlFtc4
-    let mut mpv = Command::new("mpv");
-    mpv.arg(&target).
-        arg("--input-ipc-server=/tmp/mpvsocket").
-        spawn().expect("OK");
-        let mut command1 = Command::new("echo");
-        let json = format!("{{ \"command\": [\"playlist\", \"{}\"] }}", target);
-
-        command1.arg(json);
-
-        let mut command2 = Command::new("socat");
-        command2.arg("-").arg("/tmp/mpvsocket");
-
-        let output = command1.execute_multiple_output(&mut [&mut command2]).unwrap();
-
-       
-        return content::Json(String::from_utf8(output.stdout).unwrap());
-
-    }
-
-
+/// starts a player with a target to play
+/// generates 
+/// ```sh
+/// mpv <target> --input-ipc-server=/tmp/mpvsocket
+/// ```
 pub fn event_play(target: String) -> &'static str {
-    // http://localhost:8000/load?target=/home/maren/Downloads/ytFiles/The Best Way To Practice Chords.webm
     let mut mpv = Command::new("mpv");
     mpv.arg(target).
         arg("--input-ipc-server=/tmp/mpvsocket").
@@ -55,7 +38,11 @@ pub fn event_play(target: String) -> &'static str {
     "Hello, world!"
     }
 
-
+/// resume a running instance
+/// generates 
+/// ```sh
+/// echo "{ \"command\": [\"set_property\", \"pause\", false] }}" | socat - /tmp/mpvsocket
+/// ```
 pub fn event_resume() -> content::Json<String> { 
         let mut command1 = Command::new("echo");
         command1.arg("{ \"command\": [\"set_property\", \"pause\", false] }");
@@ -68,6 +55,35 @@ pub fn event_resume() -> content::Json<String> {
 
     }
 
+/// Stops the current video and starts with the new source
+/// generates 
+/// ```sh
+/// echo "{ \"command\": [\"loadfile\", \"<target>\"] }}" | socat - /tmp/mpvsocket
+/// ```
+pub fn event_load(target: String) -> content::Json<String> {
+    let mut mpv = Command::new("mpv");
+    mpv.arg(&target).
+        arg("--input-ipc-server=/tmp/mpvsocket").
+        spawn().expect("OK");
+        let mut command1 = Command::new("echo");
+        let json = format!("{{ \"command\": [\"loadfile\", \"{}\"] }}", target);
+
+        command1.arg(json);
+
+        let mut command2 = Command::new("socat");
+        command2.arg("-").arg("/tmp/mpvsocket");
+
+        let output = command1.execute_multiple_output(&mut [&mut command2]).unwrap();
+
+       
+        return content::Json(String::from_utf8(output.stdout).unwrap());
+
+    }
+/// Pause a video
+/// generates 
+/// ```sh
+/// echo "{ \"command\": [\"set_property\", \"pause\", true] }}" | socat - /tmp/mpvsocket
+/// ```
     pub fn event_pause() -> content::Json<String>{
         let mut command1 = Command::new("echo");
         command1.arg("{ \"command\": [\"set_property\", \"pause\", true] }");
