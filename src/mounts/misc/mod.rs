6bbe30d3e6528328b05ec44eb66b1a2946bacf3c
Maren Afflerbach <maren@afflerbach.info>
2021-01-11 19:54:22 +0100

    implement shutdown endpoint

--- src/mounts/misc/mod.rs
+++ src/mounts/misc/mod.rs
@@ -1,6 +1,18 @@
 use actix_web::{ HttpResponse};
+use std::process::Command;
 
 pub async fn heartbeat() -> HttpResponse {
     let tjson = json!({ "alive": "true" });
+    return HttpResponse::Ok().json(tjson)
+}
+
+
+pub async fn shutdown() -> HttpResponse {
+    let mut mpv = Command::new("shutdown");
+    mpv.arg("-h")
+        .arg("now")
+        .spawn()
+        .expect("OK");
+    let tjson = json!({ "shutdown": "triggered" });
     return HttpResponse::Ok().json(tjson)
 }
